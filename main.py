e26b4c036f5813fc6db1812b0a35772c4a557dd3
narenakash <naren.akash@research.iiit.ac.in>
2023-11-06 02:01:08 +0530

    model trains, auxiliary functions needs to be added - wip

--- main.py
+++ main.py
@@ -1,18 +1,63 @@
 import wandb
 
-from utils import get_config
+import torch
+from torch import nn
+from torch import optim
+from torch.utils.data import DataLoader
+
+from models.unet import UNet
+# from losses import DiceCELoss
+from dataset import OPMDDataset
+from utils import get_config, set_seed
+
+from train import train
+# from test import test
+
+from monai.losses import DiceCELoss
 
 config = get_config("config.yaml")
 
 
+
 def main():
 
-    run()
+    set_seed(config["seed"])
+
+    # device
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    # dataset
+    train_dataset = OPMDDataset(data_csv_path=config["dataset"]["train_csv_path"], image_dir=config["dataset"]["train_img_dir"], 
+                                mask_dir=config["dataset"]["train_mask_dir"], mode="train", transform=None)
+    train_loader = DataLoader(train_dataset, batch_size=config["batch_size"], shuffle=True, num_workers=config["num_workers"])
+
+    val_dataset = OPMDDataset(data_csv_path=config["dataset"]["val_csv_path"], image_dir=config["dataset"]["val_img_dir"],
+                                mask_dir=config["dataset"]["val_mask_dir"], mode="val", transform=None)
+    val_loader = DataLoader(val_dataset, batch_size=config["batch_size"], shuffle=False, num_workers=config["num_workers"])
+
+    test_dataset = OPMDDataset(data_csv_path=config["dataset"]["test_csv_path"], image_dir=config["dataset"]["test_img_dir"],
+                                mask_dir=config["dataset"]["test_mask_dir"], mode="test", transform=None)
+    test_loader = DataLoader(test_dataset, batch_size=config["batch_size"], shuffle=False, num_workers=config["num_workers"])
+
+    # model
+    model = UNet(n_channels=config["model"]["n_channels"], n_classes=config["model"]["n_classes"]).to(device)
+    model = nn.DataParallel(model)
+
+    # loss and optimizer
+    criterion = DiceCELoss()
+    optimizer = optim.Adam(model.parameters(), lr=config["init_lr"])
+
+
+    # train, eval and test
+    train(model=model, train_loader=train_loader, val_loader=val_loader, optimizer=optimizer, criterion=criterion, save_dir=config["save_dir"], n_epochs=config["n_epochs"], save_freq=config["save_freq"], device=device)
+    
+    # test(test_loader, model, criterion)
+
 
     
 if __name__ == "__main__":
     
-    project_name = 'opmd-oralcavity-segmentation'
+    project_name = config["project_name"]
 
     wandb.init(
         project=project_name,
@@ -21,5 +66,7 @@ if __name__ == "__main__":
     )
 
     wandb.config.update(config)
+
+    main()
 
     wandb.finish()
--- train.py
+++ train.py
@@ -1,20 +1,68 @@
 import torch
-import torch.nn as nn
-import torch.optim as optim
 
+import time
 from tqdm import tqdm
 
-from dataset import OPMDDataset
-
-from utils import (
-    load_checkpoint,
-    save_checkpoint,
-)
+# from utils import (
+#     load_checkpoint,
+#     save_checkpoint,
+# )
 
 
-def train(loader, model, optimizer, criterion, scaler):
+def train(model, train_loader, val_loader, optimizer, criterion, save_dir, device, n_epochs=10, save_freq=1):
     
-    for batch_idx, (data, targets) in enumerate(tqdm(loader)):
-        data = data.to(device)
-        targets = targets.float()
+    for epoch in range(n_epochs):
+        train_epoch(train_loader, model, optimizer, criterion, device)
 
+        eval_epoch(val_loader, model, criterion, device)
+        
+        # if epoch % save_freq == 0:
+        #     save_checkpoint(model, optimizer, save_dir, epoch)
+        #     print(f"Checkpoint saved at {save_dir}")
+
+        # if n_gpus > 1:
+        #     torch.save(model.module.state_dict(), f"{save_dir}/model_{epoch}.pth")
+        # else:
+        #     torch.save(model.state_dict(), f"{save_dir}/model_{epoch}.pth")
+
+        # print(f"Epoch {epoch} completed. Checkpoint saved at {save_dir}")
+
+        # val_loss = eval_epoch(val_loader, model, criterion, scaler)
+        # print(f"Validation loss: {val_loss}")
+
+
+def train_epoch(loader, model, optimizer, criterion, device):
+    model.train()
+
+    losses = []
+    
+    for batch_idx, (images, masks) in enumerate(tqdm(loader, total=len(loader))):        
+        images = images.to(device)
+        masks = masks.float().to(device)
+        
+        masks_pred = model(images)
+
+        loss = criterion(masks_pred, masks)
+        
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        losses.append(loss.item())
+
+
+def eval_epoch(loader, model, criterion, device):
+    model.eval()
+
+    losses = []
+
+    with torch.no_grad():
+        for batch_idx, (images, masks) in enumerate(tqdm(loader, total=len(loader))):
+            images = images.to(device)
+            masks = masks.float().to(device)
+
+            masks_pred = model(images)
+
+            loss = criterion(masks_pred, masks)
+
+            losses.append(loss.item())
