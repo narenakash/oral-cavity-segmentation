33c8175fbb7cf15ac1478521d801a4d1c14fb852
narenakash <naren.akash@research.iiit.ac.in>
2023-11-15 06:50:02 +0530

    compute epoch mean metric and loss and log to wandb - code added but not tested

--- train.py
+++ train.py
@@ -1,37 +1,53 @@
 import torch
 
 import time
+import wandb
 from tqdm import tqdm
 
-# from utils import (
-#     load_checkpoint,
-#     save_checkpoint,
-# )
+from monai.metrics import DiceMetric
+
+from utils import (
+    load_checkpoint,
+    save_checkpoint,
+)
 
 
-def train(model, train_loader, val_loader, optimizer, criterion, save_dir, device, n_epochs=10, save_freq=1):
+def train(model, train_loader, val_loader, optimizer, criterion, save_dir, device, n_epochs=10, save_freq=1, n_gpus=4):
+
+    best_val_dsc = -1
+    best_val_dsc_epoch = -1
+
+    dice_metric = DiceMetric(include_background=True, reduction="mean")
     
     for epoch in range(n_epochs):
-        train_epoch(train_loader, model, optimizer, criterion, device)
 
-        eval_epoch(val_loader, model, criterion, device)
+        # train_epoch and eval_epoch functions
+        train_loss, train_dsc = train_epoch(train_loader, model, optimizer, criterion, dice_metric, device)
+
+        val_loss, val_dsc = eval_epoch(val_loader, model, criterion, dice_metric, device)
+
+        if val_dsc > best_val_dsc:
+            best_val_dsc = val_dsc
+            best_val_dsc_epoch = epoch + 1
+
+            save_checkpoint(model, optimizer, save_dir, epoch+1)
+            print(f"New best DSC metric checkpoint saved at {save_dir}")
+
+        print("Current epoch: {} current mean dice: {:.4f} best mean dice: {:.4f} at epoch {}".format(
+                        epoch + 1, val_dsc, best_val_dsc, best_val_dsc_epoch))
         
+        # save_checkpoint function
         # if epoch % save_freq == 0:
         #     save_checkpoint(model, optimizer, save_dir, epoch)
-        #     print(f"Checkpoint saved at {save_dir}")
+        #     print(f"Epoch {epoch} completed. Checkpoint saved at {save_dir}")
 
         # if n_gpus > 1:
         #     torch.save(model.module.state_dict(), f"{save_dir}/model_{epoch}.pth")
         # else:
         #     torch.save(model.state_dict(), f"{save_dir}/model_{epoch}.pth")
 
-        # print(f"Epoch {epoch} completed. Checkpoint saved at {save_dir}")
 
-        # val_loss = eval_epoch(val_loader, model, criterion, scaler)
-        # print(f"Validation loss: {val_loss}")
-
-
-def train_epoch(loader, model, optimizer, criterion, device):
+def train_epoch(loader, model, optimizer, criterion, dice_metric, device):
     model.train()
 
     losses = []
@@ -48,10 +64,23 @@ def train_epoch(loader, model, optimizer, criterion, device):
         loss.backward()
         optimizer.step()
 
+        print(f"{batch_idx}/{len(loader)}, train_loss: {loss.item():.4f}")
         losses.append(loss.item())
 
+        dice_metric(y_pred=masks_pred, y=masks)
+    
+    dice_score = dice_score.aggregate().item()
+    dice_metric.reset()
 
-def eval_epoch(loader, model, criterion, device):
+    loss = sum(losses) / len(losses)
+    wandb.log({"train_loss": loss, "train_dice_score": dice_score})
+
+    print(f"train_loss: {loss:.4f}, train_dice_score: {dice_score:.4f}")
+
+    return loss, dice_metric
+
+
+def eval_epoch(loader, model, criterion, dice_metric, device):
     model.eval()
 
     losses = []
@@ -66,3 +95,15 @@ def eval_epoch(loader, model, criterion, device):
             loss = criterion(masks_pred, masks)
 
             losses.append(loss.item())
+
+            dice_metric(y_pred=masks_pred, y=masks)
+
+    dice_score = dice_score.aggregate().item()
+    dice_metric.reset()
+
+    loss = sum(losses) / len(losses)
+    wandb.log({"val_loss": loss, "val_dice_score": dice_score})
+
+    print(f"val_loss: {loss:.4f}, val_dice_score: {dice_score:.4f}")
+
+    return loss, dice_metric
